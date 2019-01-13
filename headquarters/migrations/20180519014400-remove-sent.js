803f1c8d07548644aa0a99ad4bcfb82bf3726306
Gabe Smedresman <gabesmed@yahoo.com>
2019-01-13 00:12:38 -0800

    clear old migrations

--- headquarters/migrations/20180519014400-remove-sent.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-  up: async (queryInterface) => {
-    await queryInterface.removeColumn('message', 'sent_at');
-  },
-  down: async (queryInterface) => {
-    await queryInterface.addColumn('message', 'sent_at', { type: 'DATETIME' });
-  }
-};
