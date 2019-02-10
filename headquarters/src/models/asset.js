fb66033d2ec26d302245a25576fed60959959553
Gabe Smedresman <gabesmed@yahoo.com>
2019-02-09 20:07:11 -0800

    add asset test

--- headquarters/src/models/asset.js
+++ headquarters/src/models/asset.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+const jsonschema = require('jsonschema');
+const { ValidationError } = require('sequelize');
+
 const database = require('../config').database;
 const Experience = require('./experience');
 const Org = require('./org');
@@ -13,7 +17,70 @@ const {
   snakeCaseColumns
 } = require('../sequelize/fields');
 
-const ASSET_TYPE_OPTIONS = ['directions'];
+const STRING_SCHEMA = { type: 'string', minLength: 1 };
+
+const COORD_SCHEMA = {
+  type: 'array',
+  items: { type: 'number' },
+  minItems: 2,
+  maxItems: 2
+};
+
+const DIRECTIONS_SCHEMA = {
+  type: 'object',
+  properties: {
+    route: STRING_SCHEMA,
+    from_option: STRING_SCHEMA,
+    to_option: STRING_SCHEMA,
+    start: COORD_SCHEMA,
+    end: COORD_SCHEMA,
+    steps: {
+      type: 'array',
+      minItems: 1,
+      items: {
+        type: 'object',
+        properties: {
+          start: COORD_SCHEMA,
+          instructions: STRING_SCHEMA,
+          distance: STRING_SCHEMA
+        },
+        required: ['start', 'instructions', 'distance'],
+        additionalProperties: false
+      }
+    },
+    polyline: STRING_SCHEMA
+  },
+  required: ['route', 'from_option', 'to_option', 'start', 'end', 'steps',
+    'polyline'],
+  additionalProperties: false
+};
+
+const ASSET_DATA_SCHEMAS = {
+  directions: DIRECTIONS_SCHEMA
+};
+
+function assetValidator(type) {
+  return function() {
+    if (type !== this.type) {
+      return;
+    }
+    const schema = ASSET_DATA_SCHEMAS[this.type];
+    const validator = new jsonschema.Validator();
+    const options = { propertyName: 'data' };
+    const result = validator.validate(this.data, schema, options);
+    if (!result.valid) {
+      const errorMessages = result.errors
+        .map(e => `${e.property} ${e.message}`)
+        .join('; ');
+      throw new ValidationError(errorMessages, result.errors);
+    }
+  };
+}
+
+const assetValidators = _(Object.keys(ASSET_DATA_SCHEMAS))
+  .map(type => [type, assetValidator(type)])
+  .fromPairs()
+  .value();
 
 /**
  * Asset model
@@ -21,11 +88,13 @@ const ASSET_TYPE_OPTIONS = ['directions'];
 const Asset = database.define('Asset', snakeCaseColumns({
   createdAt: datetimeField(),
   updatedAt: mutableModifier(datetimeField()),
-  type: enumStringField(32, ASSET_TYPE_OPTIONS),
+  type: enumStringField(32, Object.keys(ASSET_DATA_SCHEMAS)),
   name: mutableModifier(requiredStringField(64)),
   data: mutableModifier(jsonField(database, 'Asset', 'data')),
   isArchived: mutableModifier(booleanField(false))
-}));
+}), {
+  validate: assetValidators
+});
 
 Asset.belongsTo(Org, belongsToField('org'));
 Asset.belongsTo(Experience, belongsToField('experience'));
