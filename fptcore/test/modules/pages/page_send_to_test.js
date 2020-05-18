7bb53a26e33ac5c81bd3e032062ffdc0011c8a95
Gabe Smedresman <gabesmed@yahoo.com>
2020-05-18 13:28:32 -0700

    add tests

--- fptcore/test/modules/pages/page_send_to_test.js
+++ fptcore/test/modules/pages/page_send_to_test.js
@@ -2,20 +2,20 @@ const assert = require('assert');
 
 const send_to_page = require('../../../src/modules/pages/page_send_to');
 
-describe('#send_to_page', () => {
-  const actionContext = {
-    scriptContent: {
-      roles: [{ name: 'Tablet', actor: false }],
-      pages: [{ name: 'PAGE-ONE' }, { name: 'PAGE-ZERO' }]
-    },
-    evalContext: {
-      tripState: {
-        currentSceneName: 'SCENE',
-        currentPageNamesByRole: { Tablet: 'PAGE-ZERO' }
-      }
+const actionContext = {
+  scriptContent: {
+    roles: [{ name: 'Tablet', actor: false }],
+    pages: [{ name: 'PAGE-ONE' }, { name: 'PAGE-ZERO' }]
+  },
+  evalContext: {
+    tripState: {
+      currentSceneName: 'SCENE',
+      currentPageNamesByRole: { Tablet: 'PAGE-ZERO' }
     }
-  };
+  }
+};
 
+describe('#send_to_page', () => {
   it('sends to page', () => {
     const params = { role_name: 'Tablet', page_name: 'PAGE-ONE' };
 
@@ -45,6 +45,42 @@ describe('#send_to_page', () => {
           currentPageNamesByRole: { Tablet: '' }
         }
       }
+    }]);
+  });
+
+  it('sends current role to page if supplied', () => {
+    const params = { role_name: 'current', page_name: 'PAGE-ONE' };
+    const actionContextWithEvent = Object.assign({}, actionContext, {
+      evalContext: Object.assign({}, actionContext.evalContext, {
+        event: { role_name: 'CurrentRole' }
+      })
+    });
+
+    const res = send_to_page.getOps(params, actionContextWithEvent);
+
+    assert.deepEqual(res, [{
+      operation: 'updateTripFields',
+      fields: {
+        tripState: {
+          currentSceneName: 'SCENE',
+          currentPageNamesByRole: {
+            Tablet: 'PAGE-ZERO',
+            CurrentRole: 'PAGE-ONE'
+          }
+        }
+      }
+    }]);
+  });
+
+  it('logs error if current role is not supplied', () => {
+    const params = { role_name: 'current', page_name: 'PAGE-ONE' };
+
+    const res = send_to_page.getOps(params, actionContext);
+
+    assert.deepEqual(res, [{
+      operation: 'log',
+      level: 'error',
+      message: 'No current role in event when expected.'
     }]);
   });
 });
