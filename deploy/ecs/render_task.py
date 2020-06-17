e6893796b96d9699161444aaca0f5dff29e731b9
Gabe Smedresman <gabesmed@yahoo.com>
2020-06-17 16:59:53 -0700

    str

--- deploy/ecs/render_task.py
+++ deploy/ecs/render_task.py
@@ -10,13 +10,13 @@ registry = '875382849197.dkr.ecr.us-west-2.amazonaws.com'
 def main(env_name, git_hash):
     task_path = os.path.join(os.path.dirname(__file__), 'task.yaml')
     task_data = yaml.safe_load(open(task_path))
-    task_data['cpu'] = int(task_data['cpu'])
-    task_data['memory'] = int(task_data['memory'])
+    task_data['cpu'] = str(task_data['cpu'])
+    task_data['memory'] = str(task_data['memory'])
     env_path = os.path.join(os.path.dirname(__file__), '{}.yaml'.format(env_name))
     env_data = yaml.safe_load(open(env_path))
     for container_data in task_data['containerDefinitions']:
-        container_data['cpu'] = int(container_data['cpu'])
-        container_data['memory'] = int(container_data['memory'])
+        container_data['cpu'] = str(container_data['cpu'])
+        container_data['memory'] = str(container_data['memory'])
         container_data['environment'] = env_data['environment']
         container_data['secrets'] = env_data['secrets']
     image_url = '{}/charter:{}'.format(registry, git_hash)
