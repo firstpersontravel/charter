312bae1e0f2d83b567408a7e2dc5d4ae1b6c6918
Gabe Smedresman <gabesmed@yahoo.com>
2020-06-17 16:53:03 -0700

    cpu to string

--- deploy/ecs/render_task.py
+++ deploy/ecs/render_task.py
@@ -12,7 +12,11 @@ def main(env_name, git_hash):
     task_data = yaml.safe_load(open(task_path))
     env_path = os.path.join(os.path.dirname(__file__), '{}.yaml'.format(env_name))
     env_data = yaml.safe_load(open(env_path))
+    env_data['cpu'] = int(env_data['cpu'])
+    env_data['memory'] = int(env_data['memory'])
     for container_data in task_data['containerDefinitions']:
+        container_data['cpu'] = int(container_data['cpu'])
+        container_data['memory'] = int(container_data['memory'])
         container_data['environment'] = env_data['environment']
         container_data['secrets'] = env_data['secrets']
     image_url = '{}/charter:{}'.format(registry, git_hash)
