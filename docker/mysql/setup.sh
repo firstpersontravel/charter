2994aaa93275b85a57f036d5933fc28273b8cb7e
Gabe Smedresman <gabe@benchling.com>
2020-07-30 11:37:33 -0700

    remove db backup and bootstrap db on start

--- docker/mysql/setup.sh
+++ docker/mysql/setup.sh
@@ -23,6 +23,3 @@ echo "FLUSH PRIVILEGES" | $MYSQL_CMD
 
 echo "CREATE DATABASE galaxy;" | $MYSQL_CMD
 echo "CREATE DATABASE galaxy_test;" | $MYSQL_CMD
-
-# Restore backup
-$MYSQL_CMD galaxy < /docker-entrypoint-initdb.d/backup-2020-05-19.bak
