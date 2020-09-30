5c049eea9ed3daf025dd4b2b221d0dd97b737991
Gabe Smedresman <gabesmed@yahoo.com>
2020-09-30 02:06:15 -0700

    fix log level for play clip

--- fptcore/src/modules/calls/clip_play.js
+++ fptcore/src/modules/calls/clip_play.js
@@ -39,7 +39,7 @@ module.exports = {
     } else {
       return [{
         operation: 'log',
-        level: 'warning',
+        level: 'warn',
         message: 'No transcript or audio for clip.'
       }];
     }
