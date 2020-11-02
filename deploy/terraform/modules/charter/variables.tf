4da553108254db3ca64655ea545ae09dea385a7d
Gabe Smedresman <gabesmed@yahoo.com>
2020-11-01 17:20:43 -0800

    Remove infra (#39)
    Remove infra from main

--- deploy/terraform/modules/charter/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "environment_name" {
-  type = string
-}
-
-variable "container_definitions" {
-  type = string
-}
--- deploy/terraform/modules/charter/vpc.tf
+++
@@ -1,7 +0,0 @@
-data "aws_vpc" "charter" {
-  id = "vpc-0f9f752701a23de70"
-}
-
-data "aws_subnet_ids" "charter" {
-  vpc_id = data.aws_vpc.charter.id
-}
